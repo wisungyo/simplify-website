615ea5fc436b8c46a4e199b221317340fb7d80f4
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-12-03 06:12:28 +0700

    feat: add sitemap

--- app/constants/links.ts
+++ app/constants/links.ts
@@ -2,3 +2,4 @@ export const instagram = "https://www.instagram.com/wisungyo";
 export const twitter = "https://www.twitter.com/wisungyo";
 export const linkedin = "https://www.linkedin.com/in/wisungyo";
 export const github = "https://www.github.com/wisungyo";
+export const wisungyoCom = "https://wisungyo.com";
