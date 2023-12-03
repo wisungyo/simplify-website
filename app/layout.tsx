d39faf12c3ab0fa76ff5892b5304dc6c312acf9a
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-12-03 12:53:30 +0700

    fix: seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,63 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { fullname, description, occupation } from "./constants/labels";
+import { wisungyoComs } from "./constants/links";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wisnu Agung Prasetyo",
-  description:
-    "Also called wisungyo. A mobile & frontend developer of a tech company in Jakarta called Laku6, company under Carousell Group. Working remotely from home, Blitar",
+  metadataBase: new URL(wisungyoComs),
+  title: fullname,
+  description: description,
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+  openGraph: {
+    title: fullname,
+    description: description,
+    url: wisungyoComs,
+    siteName: fullname,
+    images: [
+      {
+        url: "https://wisungyo.com/_next/image?url=%2Fimages%2Fprofile.jpeg&w=256&q=75",
+        width: 256,
+        height: 256,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};
+
+const jsonLd = {
+  "@context": "http://schema.org/",
+  "@type": "Person",
+  name: fullname,
+  jobTitle: occupation,
+  url: wisungyoComs,
 };
 
 export default function RootLayout({
@@ -17,7 +67,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dracula">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,10 @@ import Bio from "./components/Bio";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Panel from "./components/Panel";
-import StructuredData from "./components/StructuredData";
 
 export default function Home() {
-  const jsonData = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    name: "Wisnu Agung Prasetyo",
-    url: "https://wisungyo.com",
-    logo: "https://wisungyo.com/_next/image?url=%2Fimages%2Fprofile.jpeg&w=256&q=75",
-  };
-
-  const metaTags = () => (
-    <>
-      <meta
-        name="description"
-        content="Also called wisungyo. A mobile & frontend developer of a tech company in Jakarta called Laku6, company under Carousell Group. Working remotely from home, Blitar"
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:title" content="Wisnu Agung Prasetyo" />
-      <meta property="og:url" content="https://wisungyo.com/" />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:description"
-        content="Also called wisungyo. A mobile & frontend developer of a tech company in Jakarta called Laku6, company under Carousell Group. Working remotely from home, Blitar"
-      />
-      <meta
-        property="og:image"
-        content="https://wisungyo.com/_next/image?url=%2Fimages%2Fprofile.jpeg&w=256&q=75"
-      />
-      <meta property="og:image:alt" content="Wisnu Agung Prasetyo" />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:site_name" content="Wisnu Agung Prasetyo" />
-      <meta name="next-head-count" content="15" />
-    </>
-  );
-
   return (
     <>
-      <Head>
-        <link rel="canonical" href="https://wisungyo.com" />
-        {metaTags()}
-        <StructuredData data={jsonData} />
-      </Head>
-
       <main className="mx-auto max-w-2xl px-4 py-8">
         <Header />
         <Bio />
