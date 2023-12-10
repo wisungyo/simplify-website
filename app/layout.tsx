1de4b41e4922d46c70197b3b38ae1ae8976c88e0
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-12-11 01:00:56 +0700

    feat: fix SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { jsonLD, metaData } from "./constants/values";
 import { Metadata } from "next";
+import Head from "next/head";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,14 +16,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dracula">
-      <body
-        className={`${inter.className} bg-gradient-to-tr from-[#282A36] from-60% to-[#2B2E3B] to-100%`}
-      >
-        {children}
+      <Head>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLD) }}
         />
+      </Head>
+      <body
+        className={`${inter.className} bg-gradient-to-tr from-[#282A36] from-60% to-[#2B2E3B] to-100%`}
+      >
+        {children}
         <SpeedInsights />
       </body>
     </html>
