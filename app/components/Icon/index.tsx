b394f99285871cad4f94707f051c835e53bfd437
Wisnu Agung Prasetyo <wisnu.agung@laku6.com>
2024-02-11 13:01:33 +0700

    fix: add aria-label for SEO acecssibility

--- app/components/Icon/index.tsx
+++ app/components/Icon/index.tsx
@@ -4,7 +4,7 @@ import { IconFooter } from "@/app/constants/types";
 
 const Icon = ({ link, IconComponent, color }: IconFooter) => {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
+    <a href={link} target="_blank" rel="noopener noreferrer" aria-label={link}>
       <IconComponent
         size="2em"
         className="hover:text-primary duration-200 transition-all"
