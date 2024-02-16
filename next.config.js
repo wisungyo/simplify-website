2b21925e89a9e2aebc7ca436f0d1d0a8b3b77ea9
Wisnu Agung Prasetyo <wisnu.agung@laku6.com>
2024-02-16 20:41:08 +0700

    feat: add cache control

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  headers: [
-    {
-      key: "Cache-Control",
-      value: "public, max-age=31536000, immutable",
-    },
-  ],
+
+  async headers() {
+    return [
+      {
+        source: "/static/:path*", // Adjust the path pattern as needed
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
